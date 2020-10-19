02675bc37102cf8f6792c85594184c9efb6d1cf5
Sebastian Matias Segura <segurasebastian7887@gmail.com>
2020-10-19 10:11:57 -0300

    Add TailwindCSS support

---
+++ components/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HiSun, HiMoon } from 'react-icons/hi'
+import styles from './Button.module.css'
+
+const Button: React.FC = () => {
+  return (
+    <button arial-label="button" className={styles.button}>
+      <HiSun color="#f6e05e" />
+      <HiMoon color="#f605e" />
+      <span />
+    </button>
+  )
+}
+
+export default Button
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { NextPage } from 'next'
+import Button from '@components/Button'
 
 const HomePage: NextPage = () => {
   return (
     <div>
+      <Button />
       <h3>Hello World!</h3>
     </div>
   )
