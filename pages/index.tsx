4020a98a1ddc662d556b190bcc7681d8bb80c69b
Sebastian Matias Segura <segurasebastian7887@gmail.com>
2020-10-27 18:23:30 -0300

    Add Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NextPage } from 'next'
+import Image from 'next/image'
 import { PageTitle } from 'components/PageTitle'
 import { MainLayout } from 'layouts/MainLayout'
 
@@ -8,7 +9,14 @@ const IndexPage: NextPage = () => {
     <>
       <PageTitle title="Home" />
       <MainLayout>
-        <h3 className="font-sans">Hello World!</h3>
+        <h3 className="text-4xl font-extrabold">Hello World!</h3>
+        <p className="mb-4">Reality is what you can get away with.</p>
+        <Image
+          alt="Next.js logo"
+          src="/vercel.png"
+          width={400}
+          height={400}
+        />
       </MainLayout>
     </>
   )
