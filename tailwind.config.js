8e16c19f7a138d01a75ac045966ed2b44712caf3
Sebastian Matias Segura <segurasebastian7887@gmail.com>
2020-11-14 12:33:18 -0300

    Update purgecss setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,64 +2,67 @@ const plugin = require('tailwindcss/plugin')
 const selectorParser = require('postcss-selector-parser')
 
 module.exports = {
-  purge: [
-    './components/**/*.{js,jsx,ts,tsx}',
-    './layouts/**/*.{js,jsx,ts,tsx}',
-    './pages/**/*.{js,jsx,ts,tsx}'
-  ],
+  purge: {
+    enabled: true,
+    content: [
+      './components/**/*.{js,jsx,ts,tsx}',
+      './layouts/**/*.{js,jsx,ts,tsx}',
+      './pages/**/*.{js,jsx,ts,tsx}',
+    ],
+  },
   theme: {
     extend: {
       maxWidth: {
-        prose: "65ch",
+        prose: '65ch',
       },
 
       animation: {
         'spin-slow': 'spin 1.5s linear infinite',
       },
 
-      typography: (theme) => ({
+      typography: theme => ({
         dark: {
           css: {
-            color: theme("colors.gray.300"),
+            color: theme('colors.gray.300'),
             '[class~="lead"]': {
-              color: theme("colors.gray.400"),
+              color: theme('colors.gray.400'),
             },
             blockquote: {
-              color: theme("colors.gray.300"),
-              borderLeftColor: theme("colors.gray.700"),
+              color: theme('colors.gray.300'),
+              borderLeftColor: theme('colors.gray.700'),
             },
             hr: {
-              borderTopColor: theme("colors.gray.800"),
+              borderTopColor: theme('colors.gray.800'),
             },
             strong: {
-              color: theme("colors.white"),
+              color: theme('colors.white'),
             },
-            "figure figcaption": {
-              color: theme("colors.gray.500"),
+            'figure figcaption': {
+              color: theme('colors.gray.500'),
             },
             a: {
-              color: theme("colors.white"),
+              color: theme('colors.white'),
             },
             th: {
-              color: theme("colors.white"),
+              color: theme('colors.white'),
             },
-            "h1, h2, h3, h4": {
-              color: theme("colors.white"),
+            'h1, h2, h3, h4': {
+              color: theme('colors.white'),
             },
             code: {
-              color: theme("colors.gray.300"),
+              color: theme('colors.gray.300'),
             },
-            "code:before": {
-              color: theme("colors.gray.300"),
+            'code:before': {
+              color: theme('colors.gray.300'),
             },
-            "code:after": {
-              color: theme("colors.gray.300"),
+            'code:after': {
+              color: theme('colors.gray.300'),
             },
-            "ol > li:before": {
-              color: theme("colors.gray.400"),
+            'ol > li:before': {
+              color: theme('colors.gray.400'),
             },
-            "ul > li:before": {
-              backgroundColor: theme("colors.gray.600"),
+            'ul > li:before': {
+              backgroundColor: theme('colors.gray.600'),
             },
           },
         },
@@ -67,29 +70,29 @@ module.exports = {
     },
   },
   variants: {
-    margin: ["responsive", "first"],
+    margin: ['responsive', 'first'],
     fontWeight: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-    textColor: ["responsive", "dark", "hover", "focus"],
-    backgroundColor: ["responsive", "dark", "hover", "focus"],
-    typography: ["responsive", "dark"],
-    textDecoration: ["responsive", "hover", "focus", 'active'],
+    textColor: ['responsive', 'dark', 'hover', 'focus'],
+    backgroundColor: ['responsive', 'dark', 'hover', 'focus'],
+    typography: ['responsive', 'dark'],
+    textDecoration: ['responsive', 'hover', 'focus', 'active'],
   },
   plugins: [
     plugin(function ({ addVariant, prefix }) {
-      addVariant("dark", ({ modifySelectors, separator }) => {
+      addVariant('dark', ({ modifySelectors, separator }) => {
         modifySelectors(({ selector }) => {
-          return selectorParser((selectors) => {
-            selectors.walkClasses((sel) => {
-              sel.value = `dark${separator}${sel.value}`;
+          return selectorParser(selectors => {
+            selectors.walkClasses(sel => {
+              sel.value = `dark${separator}${sel.value}`
               sel.parent.insertBefore(
                 sel,
-                selectorParser().astSync(prefix(".scheme-dark "))
-              );
-            });
-          }).processSync(selector);
-        });
-      });
+                selectorParser().astSync(prefix('.scheme-dark '))
+              )
+            })
+          }).processSync(selector)
+        })
+      })
     }),
-    require("@tailwindcss/typography"),
+    require('@tailwindcss/typography'),
   ],
 }
